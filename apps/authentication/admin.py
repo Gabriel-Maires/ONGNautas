0a266f941df1bcc7673f3de0952dc110d77a31aa
weblerson <zonejr4@gmail.com>
2023-08-03 12:51:49 -0400

    feat: created personalized user from AbstractUser

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+
+    fieldsets = (
+        ('Informação Pessoal', {'fields': ('cpf',)}),
+        ('Cargos', {'fields': ('is_voluntary', 'is_supporter')}),
+    )
+
+    list_display = ('first_name', 'last_name', 'email', 'is_voluntary', 'is_supporter')
+
+    readonly_fields = ['is_voluntary', 'is_supporter', 'cpf']
--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth
+
+AUTH_USER_MODEL = 'authentication.User'
