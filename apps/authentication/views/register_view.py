9728d6bd7c3e45d02ee0680746c0312fab601bb4
diegokropotoff <kropotoff.diego@gmail.com>
2023-08-04 07:32:52 -0300

    register system updated with validators

--- apps/authentication/views/register_view.py
+++ apps/authentication/views/register_view.py
@@ -1,24 +1,14 @@
 from django.shortcuts import render
 from authentication.models.user import User
+from authentication.forms import RegisterForm
 
 def register_view(request):
     if request.method == 'GET':
         return render(request, 'register.html')
     elif request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        birth_date = request.POST.get('birth_date')
-        cpf = request.POST.get('cpf')
-        cep = request.POST.get('cep')
-        address = request.POST.get('address')
-        complement = request.POST.get('complement')
-        is_supporter = request.POST.get('supporter')
-        is_voluntary = request.POST.get('voluntary')
-        password = request.POST.get('password') if (request.POST.get('password') == request.POST.get('password_confirmation')
-                                                    ) else None
-        user = User.objects.create_user(username=name, email=email, 
-                    birth_date=birth_date, cpf=cpf, 
-                    cep=cep, address=address, 
-                    complement=complement, is_supporter=is_supporter,
-                    is_voluntary=is_voluntary, password=password)
+        register_form = RegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+        else:
+            return render(request, 'register.html', {'form':register_form})
         
