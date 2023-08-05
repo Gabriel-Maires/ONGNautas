0d53cc0c9c874c85cf969fffce72c0f650b36dda
Luciana Vivarelli <lucianavivarelli@hotmail.com>
2023-08-05 18:17:29 -0300

    making css&html changes

--- core/urls.py
+++ core/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authentication/', include('authentication.urls')),
     path('reports/', include('reports.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+# Front: Quaisquer problema com as medias, falar comigo. Não resolvam isso sozinhos
