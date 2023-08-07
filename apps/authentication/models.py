b2451fc15fdf5d8bb82094712793ced8b325a202
weblerson <zonejr4@gmail.com>
2023-08-07 15:03:44 -0400

    feat: implemented unique cpf validation

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
 
     is_voluntary = models.BooleanField(default=False)
     is_supporter = models.BooleanField(default=False)
-    cpf = models.CharField(max_length=11, blank=False, validators=[cpf_validator, no_whitespaces])
+    cpf = models.CharField(max_length=11, unique=True, blank=False, validators=[cpf_validator, no_whitespaces])
     birthdate = models.DateField(blank=False, default=timezone.now)
     cep = models.CharField(max_length=11, blank=False, default='Sem CEP', validators=[cep_validator, no_whitespaces])
     address = models.CharField(max_length=64, blank=False, default='Não definido')
