594a29c8820ba91dfc02b41f7244c86981f8932a
diegokropotoff <kropotoff.diego@gmail.com>
2023-08-07 14:46:53 -0300

    forms.py created in perfil

--- apps/perfil/views.py
+++ apps/perfil/views.py
@@ -4,7 +4,12 @@ def index_view(request):
     return render(request, 'index.html')
 
 def voluntary_view(request):
-    return render(request, 'voluntary.html')
+  user = request.user
+
+  return render(request, '.html', {'is_voluntary': user.is_voluntary})
 
 def supporter_view(request):
-    return render(request, 'supporter.html')
+  user = request.user
+
+  return render(request, '.html', {'is_supporter': user.is_supporter})
+
