13fde4f4b36716a997091ff7d310d3754461c535
weblerson <zonejr4@gmail.com>
2023-08-07 11:14:27 -0400

    chore: replaced "if" with "match case"

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -6,17 +6,20 @@ from .forms import RegisterForm
 
 
 def register_view(request):
-    if request.method == 'GET':
-        return render(request, 'register.html')
-    elif request.method == 'POST':
-        register_form = RegisterForm(request.POST)
-        if register_form.is_valid():
-            register_form.save()
 
-            return render(request, 'register.html', {'form': register_form},)
-            
-        else:
-            return render(request, 'register.html', {'form': register_form},)
+    match request.method:
+        case 'GET':
+            return render(request, 'register.html')
+        
+        case 'POST':
+            register_form = RegisterForm(request.POST)
+            if register_form.is_valid():
+                register_form.save()
+
+                return render(request, 'register.html', {'form': register_form},)
+                
+            else:
+                return render(request, 'register.html', {'form': register_form},)
 
 
 def login_view(request):
