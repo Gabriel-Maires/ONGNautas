22ab0558e397c01cb1612f2753c0bbd52f947666
diegokropotoff <kropotoff.diego@gmail.com>
2023-08-08 16:07:26 -0300

    view project_registration created

--- apps/perfil/models.py
+++ apps/perfil/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from authentication.models import User
+from authentication.models import Voluntary
 
 
 
 class VoluntaryProjects(models.Model):
 
     project_name = models.CharField(max_length=30, blank=False)
+    #TODO: AJUSTAR PARA PUXAR DOS PROJETOS ATIVOS
     hours_worked = models.IntegerField(blank=False)
 
     evidence_image = models.ImageField()
 
-    voluntary = models.ForeignKey(User, on_delete=models.CASCADE)
+    voluntary = models.ForeignKey(Voluntary, on_delete=models.CASCADE)
     confirmation = models.BooleanField(default=False)
