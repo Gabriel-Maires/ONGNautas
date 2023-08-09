fe929f765f729b742530abca991cf3a5a1ce5dc5
weblerson <zonejr4@gmail.com>
2023-08-09 13:57:16 -0400

    feat: created voluntary and project junction model

--- apps/ong/models.py
+++ apps/ong/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Project(models.Model):
+
+    title = models.CharField(_('title'), max_length=24, blank=False)
+    short_description = models.CharField(_('short description'), max_length=64, blank=False)
+    description = models.TextField(_('description'))
+    address = models.CharField(_('address'), max_length=50)
+    image = models.ImageField(_('image'), upload_to='projects')
+    is_active = models.BooleanField(_('is active'), default=True)
+    amount_spent = models.DecimalField(_('amount spent'), max_digits=6, decimal_places=2, default=0.0)
+
+    def __str__(self) -> str:
+        return self.title
--- apps/perfil/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import VoluntaryProjects
-
-
-class VoluntaryProjectsForm(forms.ModelForm):
-    model = VoluntaryProjects
-    fields = '__all__'
