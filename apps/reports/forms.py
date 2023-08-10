96fd8e19bc69da72727a4f47a2ac184c1bf91d87
weblerson <zonejr4@gmail.com>
2023-08-10 16:23:59 -0400

    feat: finalized denouncement view

--- apps/reports/forms.py
+++ apps/reports/forms.py
@@ -1,7 +1,41 @@
 from django import forms
 from .models import Report
 
+import re
+
 
 class ReportsForm(forms.ModelForm):
-    model = Report
-    fields = '__all__'
+    
+    class Meta:
+        model = Report
+        fields = [
+            'title', 'description', 'cep', 'address', 'complement', 'evidence_image'
+        ]
+
+    cep = forms.CharField(max_length=9)
+    address = forms.CharField(max_length=64)
+
+    def clean_cep(self) -> str:
+
+        return self._remove_characters('cep')
+    
+    def clean(self):
+
+        cleaned_data = super().clean()
+        cleaned_data['cep'] = self.clean_cep()
+
+        return cleaned_data
+    
+    def save(self, commit=True):
+        
+        instance = super().save(commit=False)
+        instance.cpf = self.cleaned_data.get('cep')
+
+        if commit:
+            instance.save()
+
+        return instance
+        
+    def _remove_characters(self, key: str) -> str:
+
+        return re.sub(r'[^0-9]', '', self.data[key])
--- apps/reports/models.py
+++ apps/reports/models.py
@@ -5,13 +5,16 @@ from authentication.validators import cep_validator
 class Report(models.Model):
 
     title = models.CharField(max_length=30, blank=False)
-    description = models.CharField(max_length=300, blank=False)
+    description = models.TextField()
 
-    cep = models.CharField(max_length=11, blank=False, default='Sem CEP', validators=[cep_validator])
+    cep = models.CharField(max_length=8, blank=False, default='Sem CEP', validators=[cep_validator])
     address = models.CharField(max_length=64, blank=False, default='Não definido')
     complement = models.CharField(max_length=24, blank=True)
 
-    evidence_image = models.ImageField()
+    evidence_image = models.ImageField(upload_to='denouncements')
+
+    def __str__(self) -> str:
+        return self.title
 
     class Meta:
         verbose_name = 'Denúncia'
