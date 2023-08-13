72c847c4a4933f8363a2256ba7c9afe2d13bee1c
diegokropotoff <kropotoff.diego@gmail.com>
2023-08-13 16:34:08 -0300

    RegisterNewsletterForm created

--- apps/ong/forms.py
+++ apps/ong/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Post
+from .models import Post, NewsletterUser
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'text', 'category', 'image', 'date']
+
+
+class RegisterNewsletterForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterUser
+        fields = 'email'
